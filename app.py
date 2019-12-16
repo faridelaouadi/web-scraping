4fb1b8961210d4bf09553a23761b1d20df9807d1
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-16 18:10:16 +0000

    searching for specific elements in bs4

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ import requests
 #the task is to get the planning permissions request for westminster
 url="https://idoxpa.westminster.gov.uk/online-applications/pagedSearchResults.do?action=page&searchCriteria.page=1"
 page = requests.get(url)
-print(page.status_code)
+soup = BeautifulSoup(page.text, 'html.parser')
+search_results = soup.find_all(class_='sr-only')
+print(search_results)
