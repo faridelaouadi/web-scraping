ccbb886254a2277aa8494c53bb375dd481428e06
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-19 15:07:00 +0000

    added django skeleton

---
+++ dealwebsite/deals/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. This will be the website for deal finding")
