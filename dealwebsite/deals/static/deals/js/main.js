9fa88d29f981fa0003b48976eecc5f08694b18c3
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-20 16:41:57 +0000

    loading the static files using django's system

---
+++ dealwebsite/deals/static/deals/js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+(function($) {
+	/*------------------
+		Navigation
+	--------------------*/
+	$(".main-menu").slicknav({
+        appendTo: '.header-section',
+		allowParentLinks: true,
+		closedSymbol: '<i class="fa fa-angle-right"></i>',
+		openedSymbol: '<i class="fa fa-angle-down"></i>'
+	});
+
+	$('.slicknav_nav').prepend('<li class="header-right-warp"></li>');
+    $('.header-right').clone().prependTo('.slicknav_nav > .header-right-warp');
+
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+	/*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-play').magnificPopup({
+        type: 'iframe'
+    });
+
+
+})(jQuery);
