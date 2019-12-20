f377bba71c6cb2893dfb9c32f977fafc4c5cf3c2
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-20 18:27:33 +0000

    cleaning the code by using header.html for every other page to extend from

--- dealwebsite/deals/views.py
+++ dealwebsite/deals/views.py
@@ -4,3 +4,20 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'deals/index.html')
+
+def results(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+                return redirect('/')
+
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "orders/login.html",
+                    context={"form":form})
