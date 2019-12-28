441cc784868911fbccac158e5272c66394f5d502
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-12-28 22:09:55 +0000

    made the background stretch to full height depending on screen size

--- dealwebsite/deals/views.py
+++ dealwebsite/deals/views.py
@@ -3,12 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    if request.method == 'POST':
-        price = request.POST.get('search_term')
-        return HttpResponse(f"you seached for {price}")
-
-    else:
-        return render(request, 'deals/index.html')
+    return render(request, 'deals/index.html')
 
 def about(request):
     return render(request, 'deals/aboutus.html')
